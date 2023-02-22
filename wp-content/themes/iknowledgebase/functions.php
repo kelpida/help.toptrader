5f4f764a99190caa4c7ab0711424f872b490198b
Elpida Kyriakou <elpida.k@chameleon.com.cy>
2023-02-22 09:12:03 +0200

    add new file to index page

--- wp-content/themes/iknowledgebase/functions.php
+++ wp-content/themes/iknowledgebase/functions.php
@@ -89,6 +89,13 @@ function iknowledgebase_widgets_init() {
 	) );
 }
 
+function reg_scripts() {
+    wp_enqueue_style( 'bootstrapstyle', get_template_directory_uri() . '/css/bootstrap.min.css' );
+    wp_enqueue_style( 'bootstrapthemestyle', get_template_directory_uri() . '/css/bootstrap-theme.min.css' );
+    wp_enqueue_script( 'bootstrap-script', get_template_directory_uri() . '/js/bootstrap.min.js', array(), true );
+}
+add_action('wp_enqueue_scripts', 'reg_scripts');
+
 add_action( 'widgets_init', 'iknowledgebase_widgets_init' );
 // Include Theme Info page.
 require get_template_directory() . '/includes/theme-info.php';
--- wp-content/themes/iknowledgebase/index.php
+++ wp-content/themes/iknowledgebase/index.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @package iknowledgebase
- */
+* The main template file.
+*
+* This is the most generic template file in a WordPress theme
+* and one of the two required files for a theme (the other being style.css).
+* It is used to display a page when nothing more specific matches a query.
+* E.g., it puts together the home page when no home.php file exists.
+*
+* @package iknowledgebase
+*/
 
 get_header();
 
@@ -16,60 +16,88 @@ get_header();
 ?>
 
 <section class="section">
-    <div class="container">
-        <!-- <div class="has-text-centered mb-6">
+	<div class="container">
+		<!-- <div class="has-text-centered mb-6">
 			<?php iknowledgebase_main_image(); ?>
 
-        </div> -->
-        <div class="is-max-w-2xl mx-auto">
-			   <?php get_search_form(); ?>
-        </div>
-        <div class="mx-auto pt-5">
-            <!-- <div class="box is-mobile">
-				<?php iknowledgebase_posts_sorter(); ?>
-            </div> -->
+			</div> -->
+			<div class="is-max-w-2xl mx-auto">
+				<?php get_search_form(); ?>
+			</div>
+			<div class="mx-auto pt-5">
+				<!-- <div class="box is-mobile">
+					<?php iknowledgebase_posts_sorter(); ?>
+					</div> -->
 
-			<!-- <?php if ( have_posts() ) : ?>
-                <div class="panel has-background-white">
-					<?php
+					<!-- <?php if ( have_posts() ) : ?>
+						<div class="panel has-background-white">
+						<?php
 
-					// Load posts loop.
-					while ( have_posts() ) {
-						the_post();
-						get_template_part( 'template-parts/content', 'post' );
-					}
-					?>
-                </div>
-				<?php iknowledgebase_the_posts_pagination(); ?><?php endif; ?> -->
-					<div class="categories-section">
-						<div class="category-box">
-							<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
-							<h3><a href="category/general-questions/">General Question</a></h3>
+						// Load posts loop.
+						while ( have_posts() ) {
+							the_post();
+							get_template_part( 'template-parts/content', 'post' );
+						}
+						?>
 						</div>
-						<div class="category-box">
-							<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
-											<h3><a href="category/account-verification/">Account Verification</a></h3>
-						</div>
-						<div class="category-box">
-							<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
-											<h3><a href="category/registration/">General Question</a></h3>
-						</div>
+						<?php iknowledgebase_the_posts_pagination(); ?><?php endif; ?> -->
+						<div class="categories-section">
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/general-questions/">General Questions</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/account-verification/">Account Verification</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/registration/">General Question</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/verification/">Verification</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/deposits-and-withdraws/">Deposits & Withdraws</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/platforms/">Platfroms</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/trading/">Trading</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/signal-center/">Signal Center</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/finance/">Finance</a></h3>
+							</div>
+							<div class="category-box">
+								<img src="<?php bloginfo('template_url'); ?>/images/general-questions.png">
+								<h3><a href="category/security-and-privacy/">Security & Privacy</a></h3>
+							</div>
 						
+						</div>
 					</div>
-        </div>
-    </div>
-</section>
+				</div>
+			</section>
 
-<section class="chat-blk">
-	<div class="container">
-	<div class="chat-blk-text">
-	<h6>Not finding what you are looking for?</h6>
-	<p>Chat with us or send us an email</p>
-	</div>
-	<div class="button is-info">
-		Chat with us
-	</div>
-</div>
-</section>
+			<section class="chat-blk">
+				<div class="container">
+					<div class="chat-blk-text">
+						<h6>Not finding what you are looking for?</h6>
+						<p>Chat with us or send us an email</p>
+					</div>
+					<div class="button is-info">
+						Chat with us
+					</div>
+				</div>
+			</section>
 
-<?php get_footer(); ?>
+			<?php get_footer(); ?>
